0883d2375b00a8366e1f3c13529bfa83dbffd1be
Numan Nayeem <72100315+numaan0@users.noreply.github.com>
2023-01-18 03:44:11 +0530

    updated home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,9 +15,9 @@ const Home = () => {
             I'm a Full Stack Developer
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
-            I have 8 years of experience building and desgining software.
-            Currently, I love to work on web application using technologies like
-            React, Tailwind, Next JS and GraphQL.
+            I am a recent Computer Science graduate.
+             I love to work on web applications using technologies like
+          Python, Djnago and Bootsrap.
           </p>
 
           <div>
